e4200c9fe3bfc04ab6fe6ad9c0e74746ce015239
homebitcoinminers <info@homebitcoinminers.com.au>
2026-04-04 20:33:13 +1100

    Fix circular dependency - remove HighScoreService from app.module providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -76,8 +76,7 @@ const ORMModules = [
         StratumV1JobsService,
         BTCPayService,
         BraiinsService,
-        ExternalSharesService,,
-        HighScoreService,
+        ExternalSharesService,
     ],
 })
 export class AppModule {
